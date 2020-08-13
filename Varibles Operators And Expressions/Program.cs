2556133c4c3aaf795c9d4bbddf24fa13508c6275
cj9806 <cthepyro@gmail.com>
2020-08-13 11:38:19 -0700

    add varibles practice

---
+++ Varibles Operators And Expressions/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Varibles_Operators_And_Expressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string greeting = "Hello, user!";
+            int playerScore = 0;
+            string displayScore = "Your score currently is " + playerScore + ".";
+
+            Console.WriteLine(greeting);
+            Console.WriteLine(displayScore);
+            Console.ReadKey();
+
+        }
+    }
+}
