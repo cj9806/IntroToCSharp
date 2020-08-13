425da065dd345024464db392f2a15e4f32e71049
cj9806 <cthepyro@gmail.com>
2020-08-13 16:46:55 -0700

    Added the ifStatementsExercise

---
+++ ifStatements/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ifStatements
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool playerAlive = true;
+            int invulnTimer = 5;
+            if (playerAlive)
+            {
+                Console.WriteLine("The player is still alive.");
+            }
+            else
+            {
+                Console.WriteLine("the player is dead.");
+            }
+
+            invulnTimer -= 1;
+            if (invulnTimer > 0)
+            {
+                Console.WriteLine("you have " + invulnTimer + " seconds of invulnrability left.");
+                if (playerAlive)
+                {
+                    invulnTimer -= 1;
+                }
+            }
+            else if(invulnTimer == 0)
+            {
+                Console.WriteLine("you can now take damage!");
+            }
+            else if(invulnTimer < 0)
+            {
+                invulnTimer = 0;
+            }
+        }
+    }
+}
