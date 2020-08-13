4529c5acb77c73a3c8d2b1fc1c0953de826ff3a2
cj9806 <cthepyro@gmail.com>
2020-08-12 17:37:11 -0700

    Add first files

---
+++ MyFirstProgram/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello I am World!");
+            Console.WriteLine("what is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("hello " + name + ".");
+            Console.ReadLine();
+        }
+    }
+}
