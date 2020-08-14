927ad16e78b17c9c9e9c1b30601806b3c535dbed
cj9806 <cthepyro@gmail.com>
2020-08-14 12:59:30 -0700

    Added the functions practice

---
+++ FunctionsPractice/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Functions
+{
+    class Program
+    {
+        
+        static void Main()
+        {
+            Game game = new Game();
+            game.score = 100;
+            game.Start(); //dont get hung up for over an hour by forgetting your goddamned parantheses
+        }
+
+    }
+    class Game
+    {
+        public int score = 0;
+        public void Start()
+        {
+            Console.WriteLine(score);
+            for(int i = 1; i <= 3; i++)
+            {
+                PrintScore(50);
+            }
+        }
+        public int AddToScore(int add)
+        {
+            score += add;
+            return score;
+        }
+        public void PrintScore(int add)
+        {
+            Console.WriteLine("you scored: " + AddToScore(add));
+        }
+    }
+}
