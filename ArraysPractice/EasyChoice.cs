ad286f0e4a53cb880f5cbfd3d20cc4cd1e29ba7a
cj9806 <cthepyro@gmail.com>
2020-08-20 14:27:07 -0700

    added array practice

---
+++ ArraysPractice/EasyChoice.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArraysPractice
+{
+    class EasyChoice
+    {
+        public int ForceInt()
+        {
+            int validInt = 0;
+            bool validChoice = false;            
+            while (!validChoice)
+            {
+                string keyInput = Console.ReadLine();
+                if (int.TryParse(keyInput, out int b))
+                {
+                    validInt = int.Parse(keyInput);
+                    validChoice = true;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input!");
+                }
+            }
+            return validInt;
+        }
+    }
+}
--- ArraysPractice/Program.cs
+++ ArraysPractice/Program.cs
@@ -6,7 +6,27 @@ namespace ArraysPractice
     {
         static void Main(string[] args)
         {
-
-        }
+            int[] data = new int[10];
+            for (int i = 10; i > 0; i--)
+            {
+                data[i-1] = i;
+                Console.WriteLine(data[i-1]);
+            }
+            
+            //create 4 numbers and store in an array
+            EasyChoice ec = new EasyChoice();
+            int[] custArr = new int[4];
+            for(int i = 0; i <= 3; i++)
+            {
+                Console.WriteLine($"Please select a number to go into the array, current spot: {i+1}");
+                custArr[i] = (ec.ForceInt());
+            }
+            Console.WriteLine();
+            for (int i = 4; i > 0; i--)
+            {
+                Console.WriteLine(custArr[i-1]);
+            }
+            
+        }   
     }
 }
